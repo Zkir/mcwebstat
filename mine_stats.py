9a2854746ba7942e122a371a2577a490e9cf2b25
Kirill "Zkir <mekillot@yandex.ru>
2024-02-13 21:20:51 +0300

    ds linking added

--- mine_stats.py
+++ mine_stats.py
@@ -12,13 +12,14 @@ from zwebpage import ZWebPage
 MINECRAFT_DIR = "d:/.Minecraft.1.20-paper_world_n2"
 #MINECRAFT_DIR = ".Minecraft.1.20-paper_world_n2"
 
-PERMISSIONS_FILE = MINECRAFT_DIR + "/plugins/PermissionsEx/permissions.yml" 
+
 STATS_DIR = MINECRAFT_DIR + "/world/stats"
 PLAYER_DATA_DIR = MINECRAFT_DIR +"/world/playerdata"
 
 WHITELIST_FILE = MINECRAFT_DIR +"/whitelist.json"
 BANNEDLIST_FILE = MINECRAFT_DIR +"/banned-players.json"
-
+PERMISSIONS_FILE = MINECRAFT_DIR + "/plugins/PermissionsEx/permissions.yml" 
+DS_LINKS_FILE = MINECRAFT_DIR + "/plugins/DiscordSRV/accounts.aof" 
 
 ranks={'admins3':4,'admins2':3,'admins':2,'police':1, 'default':0}
 
@@ -37,6 +38,15 @@ def  format_time(t):
 
 def format_unix_time(ts):
     return datetime.fromtimestamp(ts, UTC).strftime('%Y-%m-%d') #%H:%M:%S
+    
+def remove_color_tags(s):
+    s = s.replace('&r','') 
+    s = s.replace('&9','')
+    s = s.replace('&c','')
+    s = s.replace('&4','')
+    s = s.replace('&e','')
+    
+    return s
  
 #just a list of user files
 playerdata_file_list=glob.glob(PLAYER_DATA_DIR+"/*.dat")
@@ -54,17 +64,23 @@ pex_users=pex_permissions['users']
 
 
 # white list and banned list
-
-
- 
 with open(WHITELIST_FILE,encoding='utf-8') as f:
         whitelist = json.load(f)
 
 with open(BANNEDLIST_FILE,encoding='utf-8') as f:
         banlist = json.load(f)
+        
+# DS links 
+# it seems that now it's just a text file lines separated by space
+with open(DS_LINKS_FILE,encoding='utf-8') as f:
+        ds_links_data = f.readlines()
+ds_links ={}
+for line in ds_links_data:
+    key, value = line.strip().split(' ')
+    ds_links[value]=key # for some reason it is "ds_id minecraft_uuid"
+    
 
 #we need just UUIDs of whitelisted players.
-
 whitelist_uuids=[] 
 for record in whitelist :
     whitelist_uuids.append(record['uuid'])  
@@ -99,10 +115,8 @@ for playerdata_filename in playerdata_file_list:
     else:
         admin['group'] = 'default'
         admin['prefix'] =''
-
-    admin['prefix'] = admin['prefix'].replace('&r','') 
-    admin['prefix'] = admin['prefix'].replace('&9','')
-    admin['prefix'] = admin['prefix'].replace('&c','')
+        
+    admin['prefix'] = remove_color_tags (admin['prefix'])
 
     with open(STATS_DIR +'/' + admin['uuid'] +'.json') as f:
         stats = json.load(f)
@@ -118,6 +132,11 @@ for playerdata_filename in playerdata_file_list:
             admin['used'] = 0
             
     admin['whitelisted'] = (admin["uuid"] in whitelist_uuids)         
+    
+    if admin["uuid"] in ds_links:
+        admin['discord'] =  ds_links[admin["uuid"]]
+    else:
+        admin['discord'] = ''
 
 
     if True: #admin['group'] != 'default': 
@@ -135,13 +154,22 @@ admin_list_html=''
 admin_list_html += '<h1>Игроки сервера "Добрый король и веселые сыроежки"</h1> \n'
 
 admin_list_html += '<table class="sortable">'
-admin_list_html += '<tr><th>Имя</th><th>Первое появление</th><th>Группа</th><th>Префикс</td><th>Наигранное время</th><th>Добыто</th><th>Использовано</th><th>Последнее появление</th><th>Статус</th></tr> \n'
+admin_list_html += '<tr><th>Имя</th><th>Первое появление</th><th>Группа</th><th>Префикс</td><th>Наигранное время</th><th>Добыто</th><th>Использовано</th><th>Последнее появление</th><th>Статус</th><th>Дискорд</th></tr> \n'
 for user in admins:
     status=''
     if not user['whitelisted']:
         status +='<s>W</s>'
-    #<td>'+str(user['uuid'])+'</td>
-    admin_list_html += '<tr><td>'+str(user['name'])+'</td><td style="text-align:center">'+format_unix_time(user['first_played'])+'</td><td>'+str(user['group'])+'</td><td>'+str(user['prefix'])+'</td><td>'+str(format_time(user['play_time']))+'</td><td style="text-align: right;">'+str(user['mined'])+'</td><td style="text-align: right;">'+str(user['used'])+'</td> <td style="text-align:center">'+format_unix_time(user['last_played'])+'</td><td>'+status+'</td> </tr> \n'
+        
+    discord = '&#10004' if user['discord'] !='' else ''
+        
+    admin_list_html +=  '<tr>'  
+    #admin_list_html += '<td>'+str(user['uuid'])+'</td>'
+    admin_list_html += '<td>'+str(user['name'])+'</td><td style="text-align:center">'+format_unix_time(user['first_played'])+'</td>'\
+                       +'<td>'+str(user['group'])+'</td><td>'+str(user['prefix'])+'</td><td>'+str(format_time(user['play_time']))+'</td>'\
+                       +'<td style="text-align: right;">'+str(user['mined'])+'</td><td style="text-align: right;">'+str(user['used'])+'</td>'\
+                       +' <td style="text-align:center">'+format_unix_time(user['last_played'])+'</td><td>'+status+'</td>'\
+                       + '<td style="text-align:center">'+discord+'</td>'
+    admin_list_html += '</tr> \n'
 
 admin_list_html += '</table>'
 
@@ -153,15 +181,15 @@ page2.write()
 page1 = ZWebPage("banlist.html", "Активные баны")
 banlist_html =''
 banlist_html += '<h1>Активные баны</h1> \n'
-banlist_html += '<table class="sortable">'
+banlist_html += '<table class="sortable">\n'
 
-banlist_html +='<tr><th>Игрок</th><th>Когда забанен</th><th>Кем</th><th>Причина бана</th><th>В белом списке</th></tr>'
+banlist_html +='<tr><th>Игрок</th><th>Когда забанен</th><th>Кем</th><th>Причина бана</th><th>В белом списке</th></tr>\n'
 for ban in banlist:
     whitelisted = (ban["uuid"] in whitelist_uuids) 
     if whitelisted:
-        banlist_html +='<tr><td>'+ban["name"]+'</td><td>'+ban["created"]+'</td><td>'+ban["source"]+'</td><td>'+ban["reason"]+'</td><td>'+str(whitelisted)+'</td></tr>'
+        banlist_html +='<tr><td>'+ban["name"]+'</td><td>'+ban["created"]+'</td><td>'+remove_color_tags(ban["source"].replace('§','&'))+'</td><td>'+ban["reason"]+'</td><td>'+str(whitelisted)+'</td></tr> \n'
 
-banlist_html += '</table>'
+banlist_html += '</table>\n'
 
 page1.print(banlist_html)
 page1.write()
