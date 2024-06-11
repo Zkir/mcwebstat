857a4fd507be8c497d98ce8535590e2facd4dbab
Kirill "Zkir <mekillot@yandex.ru>
2024-06-11 18:48:40 +0300

    rules and doces are published

--- update_player_stat.bat
+++ update_player_stat.bat
@@ -1,2 +1,5 @@
 python mine_stats.py
+python publish_md.py
 copy *.html d:\.Minecraft.1.20-paper_world_n2\plugins\dynmap\web
+copy _build\*.html d:\.Minecraft.1.20-paper_world_n2\plugins\dynmap\web
+copy docs\*.png d:\.Minecraft.1.20-paper_world_n2\plugins\dynmap\web
